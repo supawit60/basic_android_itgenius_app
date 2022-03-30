efa8627941423217811900da8b4cf9d029fc43dd
ssupawit.k <supawit@odds.team>
2022-03-30 22:55:20 +0700

    linear layout

--- app/src/main/java/com/ss/firstandroidapp/MainActivity.kt
+++ app/src/main/java/com/ss/firstandroidapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ss.firstandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ค้นหาตัวแปรจากฝั่ง xml เก็บลงตัวแปร
+        var textId = findViewById<TextView>(R.id.textView)
+        textId.text = "First Android App :)"
+
     }
 }
